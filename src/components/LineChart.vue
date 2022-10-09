580a9fdb2c8b486d2ae725ab0dc2d1912e3f1c5b
jeonghwanJay <nawhgnoejij@naver.com>
2022-10-09 20:44:01 +0900

    add : LineChart.vue

---
+++ src/components/LineChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>LineChart</h1>
+    <div>
+        <canvas id="lineChart"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+export default {
+    mounted() {
+        const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+    // eslint-disable-next-line
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+  // eslint-disable-next-line
+   const lineChart = new Chart(
+    document.getElementById('lineChart'),
+    config
+  );
+    }
+}
+</script>
+
+<style>
+
+</style>
